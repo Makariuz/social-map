2868d5ac7476908a17c77cea8e9315b9e292ff9c
Makariuz <samuel.eduardo23@hotmail.com>
2022-08-07 21:53:32 +0100

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import RoomIcon from "@mui/icons-material/Room";
+import axios from "axios";
+import Star from "@mui/icons-material/Star";
+import { format } from "timeago.js";
+
+import "./app.css";
+import mapboxgl from "mapbox-gl";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
 
 function App() {
+  const myStorage = window.localStorage;
+  const [currentUser, setCurrentUser] = useState(myStorage.getItem("user"))
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [showPopup, setShowPopup] = React.useState(true);
+  const [showPop, setShowPop] = useState(false)
+
+  const [showRegister, setShowRegister] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
+
+  const [title, setTitle] = useState(null)
+  const [desc, setDesc] = useState(null)
+  const [rating, setRating] = useState(0)
+
+  const [viewport, setViewport] = useState({
+   
+    longitude: 9,
+    latitude: 53
+  })
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  getPins();
+
+  }, []);
+
+  const handleMarkerClick = (id, lat, long) => {
+    setShowPopup(!showPopup)
+    setCurrentPlaceId(id)
+    setViewport({ longitude: long, latitude: lat})
+ 
+  };
+
+  const handleAddClick = (e) => {
+    const {lng , lat} = e.lngLat
+    setNewPlace({
+      lng,
+      lat
+    })
+    console.log(newPlace)
+  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.lng,
+    }
+
+    try {
+      const res = await axios.post(`/pins`, newPin)
+      setPins([...pins, res.data])
+      setNewPlace(null)
+
+
+    }catch(err) {
+      console.log(err)
+    }
+  }
+
+  const handleLogout = () => {
+    myStorage.removeItem("user");
+    setCurrentUser(null)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        initialViewState={{
+          longitude: viewport.longitude,
+          latitude: viewport.latitude,
+          zoom: 4,
+        }}
+      
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/makariuz/cl6i3pl01000714mpoiydz8uw"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        onDblClick={handleAddClick}
+        transitionDuration="1000"
+      >
+        {pins.map((p) => (
+          <>
+            <Marker longitude={p.long} latitude={p.lat} anchor="left"  >
+              <RoomIcon
+                style={{
+                  fontSize: visualViewport.scale * 40,
+                  color: p.username===currentUser ? "crimson": "slateblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+              />
+            </Marker>
+            {p._id === currentPlaceId && (
+              <Popup
+                longitude={p.long}
+                latitude={p.lat}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={() => setCurrentPlaceId(null)}
+
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    {Array(p.rating).fill(<Star className="star" />)}
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date"> {format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
+        {newPlace && (
+        <Popup
+                longitude={newPlace.lng}
+                latitude={newPlace.lat}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={() => setNewPlace(null)}
+
+              ><div>
+                <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input placeholder="Enter a title" onChange={(e) => setTitle(e.target.value)}/>
+                  <label>Review</label>
+                <textarea placeholder="Say something about this place."  onChange={(e) => setDesc(e.target.value)}/>
+                  <label>Rating</label>
+                <select  onChange={(e) => setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+               
+                </select>
+                  <button className="submitButton" type="submit">Submit</button>
+                </form>
+              </div></Popup>
+              )
+              }
+              
+              {currentUser ? ( 
+                <button className="buttons">
+                <button className="button instructions">Instructions</button>
+                <button className="button logout" onClick={handleLogout}>Log out</button>
+                </button>
+                
+                ) : (
+        <div className="buttons">
+        <button className="button instructions">Instructions</button>
+              <button className="button login" onClick={() => setShowLogin(true)}>Login</button>
+              <button className="button register" onClick={() => setShowRegister(true)}>Register</button>
+              </div>
+      )}
+      {showRegister &&     <Register setShowRegister={setShowRegister}/>}
+      {showLogin &&     <Login setShowLogin={setShowLogin} myStorage={myStorage} setCurrentUser={setCurrentUser}/>} 
+  
+      </Map>
+      
+     
+   
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,57 @@
+import Room from '@mui/icons-material/Room'
+import axios from 'axios'
+import { useRef, useState } from 'react'
+import './register.css'
+import CancelIcon from '@mui/icons-material/Cancel';
+
+
+export function Register({setShowRegister}) {
+
+    const[success, setSuccess] = useState(false)
+    const[error, setError] = useState(false)
+
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const newUser = {
+            username: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+
+        try{
+            await axios.post(`/register`, newUser)
+            setError(false)
+            setSuccess(true)
+
+        }catch(err) {
+            setError(true)
+        }
+
+
+    }
+
+    return(
+        <div className="registerContainer">
+            <div className="logo">
+                <Room />
+                SamPin
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter Username" ref={nameRef}/>
+                <input type="email" placeholder="Enter Email" ref={emailRef}/>
+                <input type="password" placeholder="Enter Password" ref={passwordRef}/>
+                <button className='registerBtn' type='submit'>Register</button>
+                {success && <span className='success'>Successfull. You can login now!</span>}
+                {error && <span className='failure'>Something went wrong!</span>}
+                
+            </form>
+                <CancelIcon className='registerCancel' onClick={() => setShowRegister(false)}/>
+        </div>
+    )
+}
